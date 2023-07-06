6aa267f881d280808de5f1c33c8736cb4aa1f5ba
dkalabukhov <daniil.kalabukhov@gmail.com>
2023-07-06 19:55:45 +0300

    module 1 fix

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,8 +1,4 @@
 function checkSpam(str) {
   const loverCaseString = str.toLowerCase();
-  if (loverCaseString.includes('xxx') || loverCaseString.includes('1xbet')) {
-    return true;
-  } else {
-    return false;
-  }
+  return loverCaseString.includes('xxx') || loverCaseString.includes('1xbet');
 }
