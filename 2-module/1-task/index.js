aad9ecda7063e80f87e127ac0b424c68d7bdb7f5
dkalabukhov <daniil.kalabukhov@gmail.com>
2023-07-08 20:19:52 +0300

    2 module done

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,8 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let result = 0;
+  for (key in salaries) {
+    result += salaries[key] !== Infinity && salaries[key] !== -Infinity && !isNaN(salaries[key]) && typeof salaries[key] === 'number' ? salaries[key] : 0;
+  }
+
+  return result;
 }
